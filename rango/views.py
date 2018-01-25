6971b85f25bc211f6ff2327a78b21508927ab5e5
MusketMosez <2254729m@student.gla.ac.uk>
2018-01-25 15:39:54 +0000

    end of chapter 5

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from rango.models import Category
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
+    return render(request, 'rango/index.html', context=context_dict)
+    
+
+
+def about(request):
+    context_dict = {'boldmessage': "Rango says hey there partner"}
+    return render(request, 'rango/about.html', context=context_dict)
+
