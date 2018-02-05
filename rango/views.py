c27abfc3b6906fbbc4ca189e072fe1108c0b3f6c
MusketMosez <2254729m@student.gla.ac.uk>
2018-02-05 22:10:39 +0000

    chapter 9 mid

--- rango/views.py
+++ rango/views.py
@@ -15,8 +15,10 @@ def index(request):
 
 
 def about(request):
-    context_dict = {'boldmessage': "Rango says hey there partner"}
-    return render(request, 'rango/about.html', context=context_dict)
+    print(request.method)
+    print(request.user)
+    ##context_dict = {'boldmessage': "Rango says hey there partner"}
+    return render(request, 'rango/about.html', {})
 
 def show_category(request, category_name_slug):
 
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -55,6 +55,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',)
+
 ROOT_URLCONF = 'tango_with_django_project.urls'
 
 TEMPLATES = [
@@ -99,6 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': { 'min_length': 6, }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
