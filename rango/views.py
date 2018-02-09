c67ba61a72bfee46479435febf35edfb4a20b42d
MusketMosez <2254729m@student.gla.ac.uk>
2018-02-09 15:43:05 +0000

    last chp 10

--- rango/views.py
+++ rango/views.py
@@ -126,7 +126,7 @@ def add_page(request, category_name_slug):
 def register(request):
     registered = False
 
-    if request.method == 'Post':
+    if request.method == 'POST':
         user_form = UserForm(data=request.POST)
         profile_form = UserProfileForm(data=request.POST)
 
